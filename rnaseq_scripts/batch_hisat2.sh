d140e0a3aba77db819f11fea66e1c06fd34ca5b2
Javier Pineda <javier@scientist.com>
2024-06-03 09:24:07 -0500

    hisat2 can take compressed fastq files

--- rnaseq_scripts/batch_hisat2.sh
+++ rnaseq_scripts/batch_hisat2.sh
@@ -2,14 +2,12 @@
 
 STRANDNESS=$1
 FASTQ_DIR=$2
-READ1_SUFFIX_GZ=$3
-READ2_SUFFIX_GZ=$4
+READ1_SUFFIX=$3
+READ2_SUFFIX=$4
 
 SECONDS=0
 
 HISAT2_INDEX="/root/indexes/hisat2/grch38/genome"
-READ1_SUFFIX=$(echo "$READ1_SUFFIX_GZ" | sed -r "s/.gz//g")
-READ2_SUFFIX=$(echo "$READ2_SUFFIX_GZ" | sed -r "s/.gz//g")
 
 # Tell bash to abort on error
 set -o pipefail
@@ -18,34 +16,24 @@ set -e -u
 cd "$FASTQ_DIR"
 mkdir hisat2
 
-READ1_FILES=$(ls *$READ1_SUFFIX_GZ)
+READ1_FILES=$(ls *$READ1_SUFFIX)
 for f in $READ1_FILES
 do
-  BASE_NAME="${f//$READ1_SUFFIX_GZ/}"
+  BASE_NAME="${f//$READ1_SUFFIX/}"
   echo "Processing reads for $BASE_NAME"
 
   # Check for paired-end files and decompress
-  if [ -z "$READ2_SUFFIX_GZ" ]
+  if [ -z "$READ2_SUFFIX" ]
   then
-    gunzip -k "$BASE_NAME$READ1_SUFFIX_GZ"
     READ_ARGUMENT="-U $BASE_NAME$READ1_SUFFIX"
   else
-    gunzip -k "$BASE_NAME$READ1_SUFFIX_GZ" "$BASE_NAME$READ2_SUFFIX_GZ"
-  	READ_ARGUMENT="-1 $BASE_NAME$READ1_SUFFIX -2 $BASE_NAME$READ2_SUFFIX"
+    READ_ARGUMENT="-1 $BASE_NAME$READ1_SUFFIX -2 $BASE_NAME$READ2_SUFFIX"
   fi
 
   # "--dta" is short for "--downstream-transcriptome-assembly" (i.e. StringTie)
-  hisat2 -q -p 8 --rna-strandness "$STRANDNESS" -x "$HISAT2_INDEX" $READ_ARGUMENT --dta |
+  hisat2 -q -p 8 --rna-strandness "$STRANDNESS" -x "$HISAT2_INDEX" $READ_ARGUMENT --remove-chrname --dta |
   sambamba view --sam-input --format bam --compression-level=0 /dev/stdin |
   sambamba sort -t 8 -o "${FASTQ_DIR}/hisat2/${BASE_NAME}_Sorted.bam" /dev/stdin
-
-  # Remove decompressed read files
-  if [ -z "$READ2_SUFFIX_GZ" ]
-  then
-    rm "$BASE_NAME$READ1_SUFFIX"
-  else
-    rm "$BASE_NAME$READ1_SUFFIX" "$BASE_NAME$READ2_SUFFIX"
-  fi
 
   duration=$SECONDS
   echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."

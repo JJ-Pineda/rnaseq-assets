1bba68cde6b377eb74cdd2ab0252acfd8b934812
Javier Pineda <javier@scientist.com>
2024-06-03 09:56:40 -0500

    stringtie script finally fixed and working

--- rnaseq_scripts/batch_stringtie.sh
+++ rnaseq_scripts/batch_stringtie.sh
@@ -32,15 +32,6 @@ set -e -u
 gunzip -k "$ANNOTATION"
 ANNOTATION=$(echo "$ANNOTATION" | sed -r "s/.gz//g")
 
-# # Remove GTF header lines
-# SKIP_LINES=$(grep -E '^#!' "$ANNOTATION" | wc -l)
-# START_LINE=$(echo `expr $SKIP_LINES + 1`)
-# tail -n "+$START_LINE" $ANNOTATION > no_header.gtf
-# mv no_header.gtf $ANNOTATION
-
-# # Remove "chr" prefix from all chromosomes in GTF
-# sed -i -E 's/chr([0-9]+)/\1/g' "$ANNOTATION"
-
 # Perform actual transcriptome assembly
 cd "$BAM_DIR"
 BAM_FILES=$(ls *$BAM_SUFFIX)
@@ -54,7 +45,7 @@ done
 
 # Merge StringTie outputs
 STRINGTIE_FILES=$(ls *stringtie.gtf | tr '\n' ' ' | xargs)
-stringtie -G "$ANNOTATION" -o "stringtie_transcriptome.gtf" "$STRINGTIE_FILES"
+stringtie --merge -G "$ANNOTATION" -o "stringtie_transcriptome.gtf" "$STRINGTIE_FILES"
 
 # Remove decompressed annotation file and intermediate stringtie GTF files
 rm "$ANNOTATION" *stringtie.gtf

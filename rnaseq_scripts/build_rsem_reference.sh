e714eebe4b50a31bd60bbe1db7ffa445021de294
Javier Pineda <javier@scientist.com>
2024-06-04 08:26:30 -0500

    added script to build rsem reference

---
+++ rnaseq_scripts/build_rsem_reference.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Tell bash to abort on error
+set -o pipefail
+set -e -u
+
+# "grch38" or "grcm39"
+GENOME=$1
+REF_DIR="/root/ensembl_references/$GENOME"
+
+SECONDS=0
+
+ASSEMBLY=$(ls ${REF_DIR}/*primary_assembly.fa.gz)
+ANNOTATION=$(ls ${REF_DIR}/*gtf.gz)
+
+gunzip -k "$ASSEMBLY" "$ANNOTATION"
+ASSEMBLY=$(echo "$ASSEMBLY" | sed -r "s/.gz//g")
+ANNOTATION=$(echo "$ANNOTATION" | sed -r "s/.gz//g")
+
+mkdir /root/rsem_references
+
+RSEM_REF_DIR="/root/rsem_references/$GENOME"
+mkdir "$RSEM_REF_DIR"
+
+# Fast: ~1 minute
+rsem-prepare-reference --gtf "$ANNOTATION" "$ASSEMBLY" "${RSEM_REF_DIR}/${GENOME}"
+
+# Remove uncompressed files
+rm "$ASSEMBLY" "$ANNOTATION"
+
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."

89a7bd05d4d4b1ec6cd395f4421bc3456481ebe3
Javier Pineda <javier@scientist.com>
2024-06-05 08:40:51 -0500

    fine-tune bbsplit scripts for compatibility with downstream gene fusion detection or transcriptome assembly

--- rnaseq_scripts/batch_bbsplit.sh
+++ rnaseq_scripts/batch_bbsplit.sh
@@ -9,9 +9,9 @@ READ1_SUFFIX=$2
 READ2_SUFFIX=$3
 METHOD=$4
 
-# Note: method should be either "t" for transcriptome (i.e. default) or "g" for genome
-# In most RNAseq cases, "t" should be used, whereas "g" would be used when you need to look for novel transcripts
-# But in those cases, you wouldn't be looking at data from a xenograft so BBSplit is probably not necessary at that point...
+# Note: method should be either "g" for genome (i.e. default), or "t" for transcriptome
+# If using Salmon downstream, "t" should be used whereas "g" should be used otherwise
+# The genome based method should especially be used if you care about novel transcripts or gene fusions
 # For WES, "g" must be used
 
 SECONDS=0
@@ -22,21 +22,15 @@ HUMAN_PATH=/root/ensembl_references/grch38
 MOUSE_PATH=/root/ensembl_references/grcm39
 INDEX_PATH=/root/indexes/bbsplit/grch38_grcm39
 
-mkdir "$INDEX_PATH"
-
-# Build BBSplit index (<5 minutes)
-# Note: uses ~22gb of storage for a genome-based human index (much smaller for transcriptome-based)
-if [ -z "$METHOD" ] || [ $METHOD = "t" ]
+if [ -z "$METHOD" ] || [ $METHOD = "g" ]
 then
-  HUMAN_TRANSCRIPTS=$(cd "$HUMAN_PATH" && ls *cdna.all.fa.gz)
-  MOUSE_TRANSCRIPTS=$(cd "$MOUSE_PATH" && ls *cdna.all.fa.gz)
-  bbsplit.sh -Xmx10g threads=12 build=1 path="$INDEX_PATH" ref_x="$HUMAN_TRANSCRIPTS" ref_y="$MOUSE_TRANSCRIPTS"
+  BUILD=1
+  MAXINDEL=200000
+  AMBIG2="all"
 else
-  HUMAN_GENOME=$(cd "$HUMAN_PATH" && ls *primary_assembly.fa.gz)
-  MOUSE_GENOME=$(cd "$MOUSE_PATH" && ls *primary_assembly.fa.gz)
-
-  # Note: "maxindel" parameter set to "200k" when performing for genome sequencing
-  bbsplit.sh -Xmx50g threads=12 build=1 maxindel=200k path="$INDEX_PATH" ref_x="$HUMAN_GENOME" ref_y="$MOUSE_GENOME"
+  BUILD=2
+  MAXINDEL=20   # i.e. BBSplit's default
+  AMBIG2="best" # i.e. BBSplit's default
 fi
 
 # Perform the actual read splitting
@@ -53,19 +47,16 @@ do
   then
     IN_NAME=$BASE_NAME$READ1_SUFFIX
 
-    bbsplit.sh -Xmx10g threads=12 build=1 path="$INDEX_PATH" in="$IN_NAME" basename="${BASE_NAME}_%.fastq.gz" refstats="${BASE_NAME}_stat.txt"
+    bbsplit.sh -Xmx10g threads=12 build=$BUILD path="$INDEX_PATH" maxindel=$MAXINDEL ambiguous2=$AMBIG2 in="$IN_NAME" basename="${BASE_NAME}_%.fastq.gz" refstats="${BASE_NAME}_stat.txt"
   else
     IN_NAME1=$BASE_NAME$READ1_SUFFIX
     IN_NAME2=$BASE_NAME$READ2_SUFFIX
 
     # Transcriptome benchmarks: 10gb seems to be sufficient
     # Benchmark: 12 threads over 11 CPUs --> ~21 minutes for one set of paired reads (including index creation)
-    bbsplit.sh -Xmx10g threads=12 build=1 path="$INDEX_PATH" in1="$IN_NAME1" in2="$IN_NAME2" basename="${BASE_NAME}_%_#.fastq.gz" refstats="${BASE_NAME}_stat.txt"
+    bbsplit.sh -Xmx10g threads=12 build=$BUILD path="$INDEX_PATH" maxindel=$MAXINDEL ambiguous2=$AMBIG2 in1="$IN_NAME1" in2="$IN_NAME2" basename="${BASE_NAME}_%_#.fastq.gz" refstats="${BASE_NAME}_stat.txt"
   fi
 
   duration=$SECONDS
   echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."
 done
-
-cd "$INDEX_PATH"
-rm -rf ref
---
+++ rnaseq_scripts/build_bbsplit_index.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Tell bash to abort on error
+set -o pipefail
+set -e -u
+
+# Note: genome-based --> "build 1"; transcriptome-based --> "build 2"
+METHOD=$1 # "g" for genome-based index (i.e. default) or "t"
+
+SECONDS=0
+
+# For reference: https://github.com/BioInfoTools/BBMap/blob/master/sh/bbsplit.sh
+# Path to ENSEMBL files
+HUMAN_PATH=/root/ensembl_references/grch38
+MOUSE_PATH=/root/ensembl_references/grcm39
+INDEX_PATH=/root/indexes/bbsplit/grch38_grcm39
+
+# Build the directory if it doesn't exist
+mkdir -p "$INDEX_PATH"
+
+# Benchmark: <5 minutes
+# Note: uses ~22gb of storage for a genome-based human index (much smaller for transcriptome-based)
+if [ -z "$METHOD" ] || [ $METHOD = "g" ]
+then
+  HUMAN_GENOME=$(cd "$HUMAN_PATH" && ls *primary_assembly.fa.gz)
+  MOUSE_GENOME=$(cd "$MOUSE_PATH" && ls *primary_assembly.fa.gz)
+
+  bbsplit.sh -Xmx50g threads=12 build=1 path="$INDEX_PATH" ref_x="$HUMAN_GENOME" ref_y="$MOUSE_GENOME"
+else
+  HUMAN_TRANSCRIPTS=$(cd "$HUMAN_PATH" && ls *cdna.all.fa.gz)
+  MOUSE_TRANSCRIPTS=$(cd "$MOUSE_PATH" && ls *cdna.all.fa.gz)
+  bbsplit.sh -Xmx10g threads=12 build=2 path="$INDEX_PATH" ref_x="$HUMAN_TRANSCRIPTS" ref_y="$MOUSE_TRANSCRIPTS"
+fi
+
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."

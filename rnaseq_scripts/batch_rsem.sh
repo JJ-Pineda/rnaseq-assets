515aac2f01f18029b60b241d713bac7a96e8776b
Javier Pineda <javier@scientist.com>
2024-06-10 09:29:01 -0500

    adding script to run rsem quantification

---
+++ rnaseq_scripts/batch_rsem.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Tell bash to abort on error
+set -o pipefail
+set -e -u
+
+## STRANDNESS
+# "RF" for reverse and paired-end
+# "R" for reverse and single-end
+# "FR" for forward and paired-end
+# "F" for forward and single-end
+
+RSEM_REF=$1
+BAM_DIR=$2
+BAM_SUFFIX=$3
+STRANDNESS=$4
+
+if [ $STRANDNESS = "RF" ]
+then
+  STRAND_ARG="--strandedness reverse"
+  PAIRED_ARG="--paired-end"
+elif [ $STRANDNESS = "FR" ]
+then
+  STRAND_ARG="--strandedness forward"
+  PAIRED_ARG="--paired-end"
+elif [ $STRANDNESS = "R" ]
+then
+  STRAND_ARG="--strandedness reverse"
+  PAIRED_ARG=""
+elif [ $STRANDNESS = "F" ]
+then
+  STRAND_ARG="--strandedness forward"
+  PAIRED_ARG=""
+else
+  STRAND_ARG="--strandedness none"
+  PAIRED_ARG=""
+fi
+
+SECONDS=0
+
+# Unzip RSEM fasta files
+gunzip -k $RSEM_REF/*fa.gz
+
+# Perform actual transcriptome assembly
+cd "$BAM_DIR"
+BAM_FILES=$(ls *$BAM_SUFFIX)
+
+for f in $BAM_FILES
+do
+  BASE_NAME="${f//$BAM_SUFFIX/}"
+  echo "Running quantification for $BASE_NAME"
+
+  rsem-calculate-expression \
+    -p 8 $PAIRED_ARG $STRAND_ARG \
+    --bam \
+    --no-bam-output \
+	--estimate-rspd \
+	--append-names \
+	"$f" \
+	"${RSEM_REF}/rsem" \
+	"$BASE_NAME"
+
+  duration=$SECONDS
+  echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."
+done
+
+# Remove decompressed RSEM fasta files
+rm "$RSEM_REF"/*fa

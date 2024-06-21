dd84a3094cc5b5905510cc3e8e327d80c84b3560
Javier Pineda <javier@scientist.com>
2024-06-21 13:08:40 -0500

    make salmon and star index creation more flexible

--- rnaseq_scripts/build_salmon_index.sh
+++ rnaseq_scripts/build_salmon_index.sh
@@ -4,42 +4,31 @@
 set -o pipefail
 set -e -u
 
-# "grch38" or "grcm39"
-GENOME=$1
-
-INDEX_DIR="/root/indexes/salmon"
-REF_DIR="/root/ensembl_references/$GENOME"
+GENOME_ASSEMBLY=$1
+TRANSCRIPTS=$2
+INDEX_DIR=$3
 
 SECONDS=0
 
 mkdir -p "$INDEX_DIR"
 
-cd "$REF_DIR"
-
-GENOME_ASSEMBLY=$(ls *primary_assembly.fa.gz)
-TRANSCRIPTS=$(ls *cdna.all.fa.gz)
-
-grep "^>" <(gunzip -c "$GENOME_ASSEMBLY") | cut -d " " -f 1 > decoys.txt
-sed -i.bak -e 's/>//g' decoys.txt
+grep "^>" <(gunzip -c "$GENOME_ASSEMBLY") | cut -d " " -f 1 > /tmp/decoys.txt
+sed -i.bak -e 's/>//g' /tmp/decoys.txt
 
 # Concatenate transcripts and genome targets (transcripts MUST come first)
-cat "$TRANSCRIPTS" "$GENOME_ASSEMBLY" > gentrome.fa.gz
+cat "$TRANSCRIPTS" "$GENOME_ASSEMBLY" > /tmp/gentrome.fa.gz
 
 # Build Salmon index (takes ~21 minutes and creates ~15gb worth of stuff)
 # If using gencode references, "--gencode" flag is required for removing extra metadata in the target header
 # "--threads" parameter specifies number of threads to use for index creation
 # "-k" parameter specifies k-mer size (defaulted to 31 which is best for reads >=75bp)
-cd "$INDEX_DIR"
-
 salmon index \
-  -t "${REF_DIR}/gentrome.fa.gz" \
-  --decoys "${REF_DIR}/decoys.txt" \
+  -t /tmp/gentrome.fa.gz \
+  --decoys /tmp/decoys.txt \
   --threads 12 \
-  --index "$GENOME"
+  --index "${INDEX_DIR}/index"
 
-echo "Created Salmon index from $GENOME transcriptome using genome for decoys"
-
-cd "$REF_DIR" && rm decoys.txt decoys.txt.bak gentrome.fa.gz
+rm /tmp/decoys.txt /tmp/decoys.txt.bak /tmp/gentrome.fa.gz
 
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."
--- rnaseq_scripts/build_star_index.sh
+++ rnaseq_scripts/build_star_index.sh
@@ -5,20 +5,17 @@ set -o pipefail
 set -e -u
 
 # "grch38" or "grcm39"
-GENOME=$1
-
-STAR_INDEX="/root/indexes/star/$GENOME"
-REF_DIR="/root/ensembl_references/$GENOME"
+GENOME_ASSEMBLY=$1
+ANNOTATION=$2
+STAR_INDEX=$3
 
 mkdir -p "$STAR_INDEX"
 
 SECONDS=0
 
-ASSEMBLY=$(ls ${REF_DIR}/*primary_assembly.fa.gz)
-ANNOTATION=$(ls ${REF_DIR}/*gtf.gz)
-gunzip -k "$ASSEMBLY" "$ANNOTATION"
+gunzip -k "$GENOME_ASSEMBLY" "$ANNOTATION"
 
-ASSEMBLY=$(echo "$ASSEMBLY" | sed -r "s/.gz//g")
+GENOME_ASSEMBLY=$(echo "$GENOME_ASSEMBLY" | sed -r "s/.gz//g")
 ANNOTATION=$(echo "$ANNOTATION" | sed -r "s/.gz//g")
 
 # Takes ~30 minutes
@@ -26,11 +23,11 @@ STAR \
   --runThreadN 8 \
   --runMode genomeGenerate \
   --genomeDir "$STAR_INDEX" \
-  --genomeFastaFiles "$ASSEMBLY" \
+  --genomeFastaFiles "$GENOME_ASSEMBLY" \
   --sjdbGTFfile "$ANNOTATION" \
   --sjdbOverhang 100
 
-rm "$ASSEMBLY" "$ANNOTATION"
+rm "$GENOME_ASSEMBLY" "$ANNOTATION"
 
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds have elapsed."

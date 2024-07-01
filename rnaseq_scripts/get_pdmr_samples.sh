0ecb7b9ea6df916e7879c55fed807a40de6d6a98
Javier Pineda <javierjpineda13@gmail.com>
2024-07-01 07:06:32 -0500

    add script with sample paired-end files from pdmr

---
+++ rnaseq_scripts/get_pdmr_samples.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Melanoma
+wget https://pdmdb.cancer.gov/pdm/128128~338-R~L42~v2.0.1.4.0~RNASEQ.R1.FASTQ.gz
+wget https://pdmdb.cancer.gov/pdm/128128~338-R~L42~v2.0.1.4.0~RNASEQ.R2.FASTQ.gz
+
+wget https://pdmdb.cancer.gov/pdm/128128~338-R~L43~v2.0.1.4.0~RNASEQ.R1.FASTQ.gz
+wget https://pdmdb.cancer.gov/pdm/128128~338-R~L43~v2.0.1.4.0~RNASEQ.R2.FASTQ.gz
+
+wget https://pdmdb.cancer.gov/pdm/128128~338-R~L44NT6~v2.0.1.4.0~RNASEQ.R1.FASTQ.gz
+wget https://pdmdb.cancer.gov/pdm/128128~338-R~L44NT6~v2.0.1.4.0~RNASEQ.R2.FASTQ.gz
+
+# Colon
+wget https://pdmdb.cancer.gov/pdm/116655~072-R~AK7~v2.0.2.14.0~RNASEQ.R1.FASTQ.gz
+wget https://pdmdb.cancer.gov/pdm/116655~072-R~AK7~v2.0.2.14.0~RNASEQ.R2.FASTQ.gz
+
+wget https://pdmdb.cancer.gov/pdm/116655~072-R~AK7A15E12~v2.0.2.14.0~RNASEQ.R1.FASTQ.gz
+wget https://pdmdb.cancer.gov/pdm/116655~072-R~AK7A15E12~v2.0.2.14.0~RNASEQ.R2.FASTQ.gz
+
+wget https://pdmdb.cancer.gov/pdm/116655~072-R~AK7A20~v2.0.2.14.0~RNASEQ.R1.FASTQ.gz
+wget https://pdmdb.cancer.gov/pdm/116655~072-R~AK7A20~v2.0.2.14.0~RNASEQ.R2.FASTQ.gz

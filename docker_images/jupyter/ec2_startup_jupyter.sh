5cf67b8e041aed2896d63a7de357367142bf7ae4
Javier Pineda <javierjpineda13@gmail.com>
2024-07-10 08:20:00 -0500

    automatically determine token and url to jupyter notebook

--- docker_images/jupyter/ec2_startup_jupyter.sh
+++ docker_images/jupyter/ec2_startup_jupyter.sh
@@ -25,7 +25,14 @@ docker run -d -t -p 8888:8888 --name jupyter jupyter_image
 # Transfer necessary files to docker container
 mkdir .aws
 echo "[default]\naws_access_key_id = ${AWS_ACCESS_KEY}\naws_secret_access_key = ${AWS_SECRET_KEY}" > .aws/credentials
-docker cp .aws jupyter:/home/jovyan
+docker cp .aws jupyter:/home/jovyan/
+
+# Get localhost url to Jupyter notebook
+TOKEN=$(docker exec jupyter jupyter notebook list | grep -o 'token=[a-z0-9]\+')
+LOCAL_URL="http://localhost:8888/lab?$TOKEN"
 
 duration=$SECONDS
-echo "Set-up is complete after ~$(($duration / 60)) minutes"
+echo "Set-up is complete after ~$(($duration / 60)) minutes."
+echo "Set up an SSH tunnel locally with \"ssh -i <pem file path> -L 8888:localhost:8888 ubuntu@<ec2-instance>\""
+echo "Then use the following link to access Jupyter:"
+echo "$LOCAL_URL"

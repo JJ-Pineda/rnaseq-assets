616da297edfdbb09fe4bf453b14e07c988785c12
Javier Pineda <javier@scientist.com>
2024-07-19 16:08:34 -0500

    add qualitry trim argument for batch_bbduk.sh

--- rnaseq_scripts/batch_bbduk.sh
+++ rnaseq_scripts/batch_bbduk.sh
@@ -6,7 +6,8 @@ set -e -u
 
 FASTQ_DIR=$1
 READ1_SUFFIX=$2
-READ2_SUFFIX=$3
+READ2_SUFFIX=$3 # empty string for single-end
+QUALITY_TRIM=$4 # "true" or "false"
 
 SECONDS=0
 REF_PATH="/usr/share/java/bbmap/adapters.fa"
@@ -15,6 +16,18 @@ TRIM_TAG="_trimmed_"
 # Don't trim reads that have already been trimmed
 cd "$FASTQ_DIR"
 READ1_FILES=$(ls *$READ1_SUFFIX | grep -v $TRIM_TAG)
+
+# Set quality trim arguments
+if [ $QUALITY_TRIM = "true" ]
+then
+  QTRIM_ARGS="qtrim=r trimq=8"
+elif [ $QUALITY_TRIM = "false" ]
+then
+  QTRIM_ARGS=""
+else
+  echo "Unrecognized input for quality trim argument...Exiting"
+  exit 1
+fi
 
 for f in $READ1_FILES
 do
@@ -31,7 +44,7 @@ do
     # Adapter trimming arguments: ref=$REF_PATH ktrim=r k=23 mink=11 hdist=1
     # Quality trimming arguments should be avoided unless there's significantly low quality
     # In which case only trim on right end of read with weak trimming --> qtrim=r trimq=8
-    bbduk.sh -Xmx4g threads=12 in=$IN_NAME out=$OUT_NAME ref=$REF_PATH ktrim=rl k=23 mink=11 hdist=1
+    bbduk.sh -Xmx4g threads=12 in=$IN_NAME out=$OUT_NAME ref=$REF_PATH ktrim=rl k=23 mink=11 hdist=1 $QTRIM_ARGS
   else
     IN_NAME1=$BASE_NAME$READ1_SUFFIX
     IN_NAME2=$BASE_NAME$READ2_SUFFIX
@@ -40,7 +53,7 @@ do
 
     # Benchmark: 12 threads --> ~5 minutes to process 1 paired-end file set
     # For paired-end reads, add "tpe tbo" arguments to trim the mates consistently
-    bbduk.sh -Xmx4g threads=12 in1=$IN_NAME1 in2=$IN_NAME2 out1=$OUT_NAME1 out2=$OUT_NAME2 ref=$REF_PATH ktrim=rl k=23 mink=11 hdist=1 tpe tbo
+    bbduk.sh -Xmx4g threads=12 in1=$IN_NAME1 in2=$IN_NAME2 out1=$OUT_NAME1 out2=$OUT_NAME2 ref=$REF_PATH ktrim=rl k=23 mink=11 hdist=1 $QTRIM_ARGS tpe tbo
   fi
 
   duration=$SECONDS

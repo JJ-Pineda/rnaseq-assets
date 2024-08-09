bdacf8d35231b0bc136437f4062a7167c3b01826
Javier Pineda <javier@scientist.com>
2024-08-09 09:13:44 -0500

    also installing 'msigdbr' and added commented lines for usage tips

--- r_scripts/salmon_postprocess.r
+++ r_scripts/salmon_postprocess.r
@@ -31,7 +31,11 @@ ah <- AnnotationHub(ask = FALSE)  # if "ask = TRUE" (i.e. default), type "yes" w
 ahResults <- query(ah, c("Annotation", "EnsDb", "Homo sapiens", "112"))
 ensDbHuman112 <- ahResults[[1]]  # Retrieves the resource
 k <- keys(ensDbHuman112, keytype = "TXNAME")
-tx2gene <- select(ensDbHuman112, k, "GENENAME", "TXNAME")
+
+# We don't need to specify "AnnotationDbi" as the source for the "select" method
+# But note that the "dplyr" (which a number of Bioconductor packages use) also has a "select" method
+# If both packages are loaded, R gets confused about which "select" to use
+tx2gene <- AnnotationDbi::select(ensDbHuman112, k, "GENEID", "TXNAME")
 
 # Perform transcript count aggregation with tximport and output gene counts
 files <- file.path(data_dir, relevant_salmon_dirs, "quant.sf")
@@ -39,7 +43,7 @@ names(files) <- relevant_salmon_dirs  # Enables proper sample labeling for the t
 txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
 txi_path <- file.path(data_dir, "salmon_gene_counts.csv")
 txi_counts <- as.data.frame(txi$counts)
-txi_counts <- rownames_to_column(txi_counts, var = "gene_name")
+txi_counts <- rownames_to_column(txi_counts, var = "gene_id")
 write.csv(txi_counts, file = txi_path, row.names = FALSE)
 
 # Gear up for DESeq2
@@ -64,6 +68,11 @@ dds <- dds[keep,]
 # Run DESeq2 (with parallelization)
 dds <- DESeq(dds, parallel = TRUE)  # Actually runs DESeq2 (with parallelization)
 
+# Gene gene ID-to-gene name conversion
+k <- keys(ensDbHuman112, keytype = "GENEID")
+geneid2name <- AnnotationDbi::select(ensDbHuman112, k, "GENENAME", "GENEID")
+names(geneid2name) = c("gene_id", "gene_name")
+
 # Iteratively generate comparison results
 for (cond in other_conditions) {
   comparison <- paste("condition", cond, "vs", ref_condition, sep = "_")
@@ -73,18 +82,21 @@ for (cond in other_conditions) {
   res_fname <- paste0("salmon_deseq2_", cond, "_vs_", ref_condition, ".csv")
   res_shrunk_fname <- paste0("salmon_deseq2_", cond, "_vs_", ref_condition, "_shrunkLFC.csv")
 
-  res <- rownames_to_column(as.data.frame(res), var = "gene_name")
-  res_shrunk <- rownames_to_column(as.data.frame(res_shrunk), var = "gene_name")
+  res <- rownames_to_column(as.data.frame(res), var = "gene_id")
+  res_shrunk <- rownames_to_column(as.data.frame(res_shrunk), var = "gene_id")
+
+  res = merge(res, geneid2name, by = "gene_id")
+  res_shrunk = merge(res_shrunk, geneid2name, by = "gene_id")
 
   write.csv(res, file = file.path(data_dir, res_fname), row.names = FALSE)
-  write.csv(res, file = file.path(data_dir, res_shrunk_fname), row.names = FALSE)
+  write.csv(res_shrunk, file = file.path(data_dir, res_shrunk_fname), row.names = FALSE)
 }
 
 # # To read in tximport CSV output and convert to DESeqDataSet
 # # Note: counts must have integer values
 # txi_counts <- read.csv(txi_path, check.names = FALSE)
-# rownames(txi_counts) <- txi_counts$gene_name
-# txi_counts <- subset(txi_counts, select = -gene_name)
+# rownames(txi_counts) <- txi_counts$gene_id
+# txi_counts <- subset(txi_counts, select = -gene_id)
 # numeric_cols <- sapply(txi_counts, is.numeric)
 # txi_counts[numeric_cols] <- lapply(txi_counts[numeric_cols], as.integer)
 #
@@ -92,17 +104,33 @@ for (cond in other_conditions) {
 #
 # # To read in DESeq CSV output and convert to DESeqResult object
 # res_shrunk_df <- read.csv(file.path(data_dir, res_shrunk_fname), check.names = FALSE)
-# rownames(res_shrunk_df) <- res_shrunk_df$gene_name
-# res_shrunk_df <- subset(res_shrunk_df, select = -gene_name)
+# rownames(res_shrunk_df) <- res_shrunk_df$gene_id
+# res_shrunk_df <- subset(res_shrunk_df, select = -gene_id)
 # res_shrunk <- DESeqResults(res_shrunk_df)
 #
 # # To run GSEA
+# library("clusterProfiler")
 # library("org.Hs.eg.db")
+# library("msigdbr")
 # res_shrunk_df <- read.csv(file.path(data_dir, res_shrunk_fname), check.names = FALSE)
-# res_filt <- res_shrunk_df[!is.na(res_shrunk_df$log2FoldChange) & !is.na(res_shrunk_df$padj) & (res_shrunk_df$gene_name != ""),]
-# res_filt <- res_filt[order(-res$log2FoldChange),]
+# res_filt <- res_shrunk_df[!is.na(res_shrunk_df$log2FoldChange) & !is.na(res_shrunk_df$padj),]
+# res_filt <- res_filt[order(-res_filt$log2FoldChange),]
 # gene_list <- res_filt$log2FoldChange
-# names(gene_list) <- res_filt$gene_name
+# names(gene_list) <- res_filt$gene_id
+#
+# # Run the actual GSEA analysis
+# # Note: the default ontology is "BP" (i.e. biological processes)
+# gse <- gseGO(gene_list, ont = "BP", keyType = "ENSEMBL", OrgDb = "org.Hs.eg.db", eps = 1e-300)
+#
+# Create classic GSEA plot; "geneSetID" refers to the row in the "gse" result
+# gseaplot(gse, geneSetID = 1)
+#
+# # To run GSEA using MSigDB "hallmark" genesets
+# h_gene_sets = msigdbr(species = "human", category = "H")
+# term2gene <- h_gene_sets[,c("gs_name", "ensembl_gene")]
+# names(term2gene) <- c("term", "gene")
+# gse_msigdb <- GSEA(gene_list, TERM2GENE = term2gene)
+# gseaplot(gse_msigdb, geneSetID = 1)
 
 print("All done!")
 
